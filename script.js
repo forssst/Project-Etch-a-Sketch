50e5bfa4f085cc3a0e9be80f9fe4afb8b1a31ce2
forssst <szymonforstkl3trgwitam@gmail.com>
2025-03-18 21:33:23 +0100

    add basic version of dynamic grid

--- script.js
+++ script.js
@@ -1,7 +1,50 @@
 const plane = document.querySelector(".plane");
+const gridNum = document.querySelector(".gridNum");
+const inputVal = document.querySelector(".input");
+const btn = document.querySelector(".btn");
+const text = document.querySelector(".text");
+
+
+btn.addEventListener("click", (e) => {
+    console.log(inputVal.value);
+    if(inputVal.value > 100 ){
+        alert("No more than 100 please")
+    } else {
+    text.innerHTML = inputVal.value;
+    plane.innerHTML = `` 
+    let x = inputVal.value * inputVal.value;
+    console.log(x)
+    // box.style.width = `calc(960 / ${inputVal.value})`;
+    for(i = 1; i <= x; i++){
+        plane.innerHTML += `<div class="box"></div>`;
+       
+        
+    }
+
+    const box =document.querySelectorAll(".box");
+    box.forEach(boxs => {
+        boxs.style.width = `calc(100% / ${inputVal.value})`;
+         boxs.style.height = `calc(100% / ${inputVal.value})`;
+        })
+   
+}})
+
+  
+
+    
+
+
+// to ma 16x16 jak valu 16 jest 
 
 
 
-for(i=1;i <= 16;i++){
-plane.innerHTML+= "<div class=box>1</div>"
-}
+
+
+
+
+
+
+
+
+
+
