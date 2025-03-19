795e569ddbbaead7c59c059c974a56a223b59200
forssst <szymonforstkl3trgwitam@gmail.com>
2025-03-19 21:30:10 +0100

    add working clearButton and RainbowModeButton

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ const gridNum = document.querySelector(".gridNum");
 const inputVal = document.querySelector(".input");
 const btn = document.querySelector(".btn");
 const clearGridBut = document.querySelector(".clearGridBut")
-const box = document.querySelectorAll(".box");
+const rainbowModeBut = document.querySelector(".rainbowModeBut")
+const hovered = document.querySelector('.hovered');
 inputVal.value = 16;
+let rainbowMode = false;
 
 
 function createGrid() {
@@ -20,15 +22,28 @@ function createGrid() {
   const box = document.querySelectorAll(".box");
   box.forEach(boxs => {
     boxs.style.width = `calc(100% / ${inputVal.value})`;
-    // boxs.style.height = `calc(100% / ${inputVal.value})`;
     /// HOVER
     boxs.addEventListener("mouseover", (e) => {
-      console.log(':d')
-      e.target.classList.add("hovered")
+      if (rainbowMode == false) {
+        // e.target.classList.add("hoveredBlack")
+        e.target.style.background = 'black'
+      } else {
+        e.target.style.background = randomRgbColor();
+      }
+     
     })
   })
 }
 }
+
+rainbowModeBut.addEventListener("click", () => {
+  if (rainbowMode == false) {
+    rainbowMode = true;
+  } else if(rainbowMode == true)  {
+    rainbowMode = false
+  }
+  console.log(rainbowMode)
+})
 
 
 
@@ -41,14 +56,27 @@ function changeGrid() {
 }
 
 function clearGrid(){
-  clearGridBut.addEventListener("click", () => {
-    console.log('chuj')
     const box = document.querySelectorAll(".box");
     box.forEach(boxs => {
-      boxs.classList.remove("hovered")
+      boxs.style.background = 'none'
     })
-  })
 }
+
+clearGridBut.addEventListener("click", () => {
+  clearGrid();
+})
+
+function randomRgbColor() {
+  let r = Math.floor(Math.random() * 256); // Random between 0-255
+  let g = Math.floor(Math.random() * 256); // Random between 0-255
+  let b = Math.floor(Math.random() * 256); // Random between 0-255
+  return 'rgb(' + r + ',' + g + ',' + b + ')';
+};
+
+// function turnPenColorRainbow() {
+// )}
+
+
 
 
 
@@ -57,6 +85,7 @@ function main() {
   createGrid();
   changeGrid();
   clearGrid();
+  // turnPenColorRainbow();
 }
 
 main();
