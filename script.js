9167dc3c7af4777947abb5f243feb53cd6975049
forssst <szymonforstkl3trgwitam@gmail.com>
2025-03-21 13:20:02 +0100

    add shadingmode and eraser & func that allow only one mode to be turn on

--- script.js
+++ script.js
@@ -6,108 +6,156 @@ const clearGridBut = document.querySelector(".clearGridBut")
 const rainbowModeBut = document.querySelector(".rainbowModeBut")
 const hovered = document.querySelector('.hovered');
 const shadingModeBut = document.querySelector(".shadingModeBut")
+const eraserBut = document.querySelector(".eraserBut")
 inputVal.value = 16;
 let rainbowMode = false;
 let shadingMode = false;
+let eraser = false;
+let grid = true;
 
 
 function createGrid() {
   if (inputVal.value > 100) {
     alert("No more than 100 please")
   } else {
-  let MulGridVal = inputVal.value * inputVal.value;
-  for (i = 1; i <= MulGridVal; i++) {
-    plane.innerHTML += `<div class="box"></div>`;
-  }
-
-  // DIVISING BOXES INTO MATCHING SQUARES
-  const box = document.querySelectorAll(".box");
-  box.forEach(boxs => {
-    boxs.style.width = `calc(100% / ${inputVal.value})`;
-    /// HOVER
-    boxs.addEventListener("mouseover", (e) => {
-      if (rainbowMode == false && shadingMode == false) {
-        e.target.style.background = 'rgba(0, 0, 0, 0.99)'
-      } else if (rainbowMode == true &&  shadingMode == false) {
-        e.target.style.background = randomRgbColor();
+      let MulGridVal = inputVal.value * inputVal.value;
+      for (i = 1; i <= MulGridVal; i++) {
+        plane.innerHTML += `<div class="box"></div>`;
       }
-      if (shadingMode == true) {
-        let x = getComputedStyle(e.target).getPropertyValue('background-color');
-        let rgbaAlpha = parseFloat(x.split(',')[3])
-        let r = parseFloat(x.slice(5).split(',')[0])
-        let g = parseFloat(x.split(',')[1])
-        let b = parseFloat(x.split(',')[2])
 
-     
-        if(rgbaAlpha < 0.99){
-          e.target.style.background = `rgba(0, 0, 0, ${rgbaAlpha + 0.1})`
-        }
-        
-      }
-     
+    // DIVISING BOXES INTO MATCHING SQUARES
+    const box = document.querySelectorAll(".box");
+        box.forEach(boxs => {
+          boxs.style.width = `calc(100% / ${inputVal.value})`;
+      /// HOVER
+          boxs.addEventListener("mouseover", (e) => {
+            if (rainbowMode == false && shadingMode == false && eraser == false) {
+              e.target.style.background = 'rgba(0, 0, 0, 0.99)'
+          } else if (rainbowMode == true && shadingMode == false) {
+              e.target.style.background = randomRgbColor();
+          } else if (rainbowMode == false && shadingMode == false && eraser == true) {
+              let getRgbaString = getComputedStyle(e.target).getPropertyValue('background-color');
+              let rgbaAlpha = parseFloat(getRgbaString.split(',')[3])
+              let r = parseFloat(getRgbaString.slice(5).split(',')[0])
+              let g = parseFloat(getRgbaString.split(',')[1])
+              let b = parseFloat(getRgbaString.split(',')[2])
+
+            if (rgbaAlpha > 0) {
+              e.target.style.background = `rgba(${r} , ${g}, ${b}, ${rgbaAlpha - 0.1})`
+            }
+          } else if (rainbowMode == false && shadingMode == true && eraser == false) {
+              let getRgbaString = getComputedStyle(e.target).getPropertyValue('background-color');
+              let rgbaAlpha = parseFloat(getRgbaString.split(',')[3])
+
+            if (rgbaAlpha < 0.99) {
+              e.target.style.background = `rgba(0, 0, 0, ${rgbaAlpha + 0.09})`
+            }
+          }
+
+
+
+      })
     })
-  })
+  }
 }
-}
+
 
 rainbowModeBut.addEventListener("click", () => {
-  if (rainbowMode == false) {
-    rainbowMode = true;
-  } else if(rainbowMode == true)  {
-    rainbowMode = false
-  }
-  console.log(rainbowMode)
+  turnRainbowMode();
+  eraser == true ? turnEraser() : null;
+  shadingMode == true ? turnShadingMode() : null;
 })
-
 
 
 shadingModeBut.addEventListener("click", () => {
-  if (shadingMode == false) {
-    shadingMode = true;
-  } else if(shadingMode == true)  {
-    shadingMode = false;
-  }
-  console.log(rainbowMode)
+  turnShadingMode();
+  rainbowMode == true ? turnRainbowMode() : null;
+  eraser == true ? turnEraser() : null;
+})
+
+
+eraserBut.addEventListener('click', () => {
+  turnEraser()
+  shadingMode == true ? turnShadingMode() : null;
+  rainbowMode == true ? turnRainbowMode() : null;
 })
 
 
 
 
-
-function changeGrid() {
-  btn.addEventListener("click", () => {
-    console.log(inputVal.value);
-    plane.innerHTML = ``
-    createGrid();
-  })
+function turnRainbowMode() {
+  if (rainbowMode == false) {
+    rainbowMode = true;
+    rainbowModeBut.style.background = 'rgba(95, 236, 39, 0.99)'
+  } else if (rainbowMode == true) {
+    rainbowMode = false
+    rainbowModeBut.style.background = 'rgba(248, 88, 88, 0.99)'
+  }
 }
 
-function clearGrid(){
-    const box = document.querySelectorAll(".box");
-    box.forEach(boxs => {
-      boxs.style.background = 'rgba(255, 255, 255, 0)'
-    })
+
+function turnShadingMode() {
+  if (shadingMode == false) {
+    shadingMode = true;
+    shadingModeBut.style.background = 'rgba(95, 236, 39, 0.99)'
+  } else if (shadingMode == true) {
+    shadingMode = false;
+    shadingModeBut.style.background = 'rgba(248, 88, 88, 0.99)'
+  }
 }
+
+
+function turnEraser() {
+  if (eraser == false) {
+    eraser = true;
+    eraserBut.style.background = 'rgba(95, 236, 39, 0.99)'
+  } else if (eraser == true) {
+    eraser = false;
+    eraserBut.style.background = 'rgba(248, 88, 88, 0.99)'
+  }
+}
+
+
+function turnGrid() {
+  if (grid == false) {
+    grid = true;
+    gridBut.style.background = 'rgba(95, 236, 39, 0.99)'
+  } else if (grid == true) {
+    grid = false
+    gridBut.style.background = 'rgba(248, 88, 88, 0.99)'
+  }
+}
+
+
+
 
 clearGridBut.addEventListener("click", () => {
   clearGrid();
 })
 
+function changeGrid() {
+  btn.addEventListener("click", () => {
+    plane.innerHTML = ``
+    createGrid();
+  })
+}
+
+function clearGrid() {
+  const box = document.querySelectorAll(".box");
+  box.forEach(boxs => {
+    boxs.style.background = 'rgba(255, 255, 255, 0)'
+  })
+}
+
+
+
 function randomRgbColor() {
-  let r = Math.floor(Math.random() * 256); 
-  let g = Math.floor(Math.random() * 256); 
+  let r = Math.floor(Math.random() * 256);
+  let g = Math.floor(Math.random() * 256);
   let b = Math.floor(Math.random() * 256);
-  let a = 0.99; 
+  let a = 0.99;
   return 'rgba(' + r + ',' + g + ',' + b + ',' + a + ')';
 };
-
-function shadingColor(r,g,b) {
-
-};
-
-
-
-
 
 
 function main() {
@@ -118,11 +166,11 @@ function main() {
 
 main();
 
-////////////////////// CO ZOSTAŁO 
-// /// co zostało 
-// wygląd trzeba ogarnąc 
+////////////////////// CO ZOSTAŁO
+// /// co zostało
+// wygląd trzeba ogarnąc
 // cieniowaine v/
-// gumke  
+// gumke
 // i ogarac czemu takie lagi sa przy zmianie bo u innych nie ma a tak to geet 
 
 
